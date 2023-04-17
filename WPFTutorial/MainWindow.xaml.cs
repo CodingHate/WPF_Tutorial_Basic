320910bf52b3e226b90661de36e62a88b85888b7
hyunwoo <junghw86@quantamatrix.com>
2023-04-18 08:13:05 +0900

    project add

---
+++ WPFTutorial/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using WPFTutorial.ModelObject;
+
+namespace WPFTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyModelObject myModelObject = new MyModelObject();
+            DataContext = myModelObject;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // MainGrid
+            // xaml에서 x:name으로 설정 한 변수를 가져와서 지정
+            MainGrid.Background = Brushes.Green;
+        }
+    }
+}
---
+++ WPFTutorial/MyModelObject.cs
@@ -0,0 +1,33 @@
+﻿namespace WPFTutorial.ModelObject
+{
+    class MyModelObject
+    {
+        public MyModelObject()
+        {
+
+        }
+
+        public string Name
+        {
+            get
+            { return "who?"; }
+            set { }
+        }
+
+        public string Age
+        {
+            get
+            { return "30"; }
+            set { }
+        }
+
+        public int NameWidth
+        {
+            get
+            {
+                return 130;
+            }
+            set { }
+        }
+    }
+}

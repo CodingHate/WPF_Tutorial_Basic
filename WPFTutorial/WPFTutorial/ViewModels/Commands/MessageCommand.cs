4e1b787ad9963200816263ad4eb3fec9f92cb42a
hyunwoo <junghw86@quantamatrix.com>
2023-04-19 08:05:05 +0900

    icommand에 관하여 기본적인 내용 확인

---
+++ WPFTutorial/WPFTutorial/ViewModels/Commands/MessageCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPFTutorial.ViewModels.Commands
+{
+    class MessageCommand : ICommand
+    {
+        private Action _execute;
+        public event EventHandler CanExecuteChanged;
+
+        public MessageCommand(Action execute)
+        {
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke();
+        }
+    }
+}
---
+++ WPFTutorial/WPFTutorial/ViewModels/MessageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFTutorial.ViewModels.Commands;
+
+namespace WPFTutorial.ViewModels
+{
+    class MessageViewModel
+    {
+        public string MessageText { set; get; }
+        public MessageCommand DisplayMessageCommand { private set; get; }
+        public MessageViewModel()
+        {
+            DisplayMessageCommand = new MessageCommand(DisplayMessage);
+        }
+
+        public void DisplayMessage()
+        {
+            MessageBox.Show(MessageText);
+        }
+    }
+}

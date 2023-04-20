93d5a65ea2d55d916f4105416dcff3344fb4f94e
hyunwoo <junghw86@quantamatrix.com>
2023-04-20 10:43:59 +0900

    Converter interface를 통하여 값 자동 변경 확인 테스트

---
+++ WPFTutorial/WPFTutorial/ViewModel/TextConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WPFTutorial.ViewModel
+{
+    class TextConverter : IValueConverter
+    {
+        public TextConverter()
+        {
+
+        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // value 가져오기
+            string textValue = (string)value;
+            if (textValue.Contains("ok"))
+                return "stop";
+            else
+                return textValue;
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

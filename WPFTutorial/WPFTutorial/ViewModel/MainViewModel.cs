3156cade28acd7f79d288dbafca9277577386abb
hyunwoo <junghw86@quantamatrix.com>
2023-04-24 08:39:50 +0900

    System.Windows을 통하여 Button의 Visible 옵션 지정 확인

---
+++ WPFTutorial/WPFTutorial/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+
+namespace WPFTutorial.ViewModel
+{
+
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            isVisible = true;
+            
+        }
+
+        private bool isVisible;
+
+        public bool IsVisible
+        {
+            get { return isVisible; }
+            set
+            {
+                isVisible = value;
+                UpdateVisibility(value);
+                OnPropertyChanged("IsVisible");
+            }
+        }
+
+        private Visibility visibility;
+        public Visibility Visibility
+        {
+            get { return visibility; }
+            set
+            {
+                visibility = value;
+                OnPropertyChanged("Visibility");
+            }
+        }
+
+
+
+        private void UpdateVisibility(bool isVisible)
+        {
+            if (isVisible)
+                Visibility = Visibility.Visible;
+            else
+                Visibility = Visibility.Hidden;
+        }
+    }
+}

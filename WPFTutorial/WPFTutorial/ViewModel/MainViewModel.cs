edc849cbcc78dc1026bd7ed8d27bc2f63aba5aec
hyunwoo <junghw86@quantamatrix.com>
2023-04-24 08:47:19 +0900

    DataContext에서 IValueConverter을 통하여 value converter 구현

--- WPFTutorial/WPFTutorial/ViewModel/MainViewModel.cs
+++ WPFTutorial/WPFTutorial/ViewModel/MainViewModel.cs
@@ -1,48 +1,48 @@
-﻿using System.Windows;
+﻿//using System.Windows;
 
-namespace WPFTutorial.ViewModel
-{
+//namespace WPFTutorial.ViewModel
+//{
 
-    class MainViewModel : ObservableObject
-    {
-        public MainViewModel()
-        {
-            isVisible = true;
+//    class MainViewModel : ObservableObject
+//    {
+//        public MainViewModel()
+//        {
+//            isVisible = true;
             
-        }
+//        }
 
-        private bool isVisible;
+//        private bool isVisible;
 
-        public bool IsVisible
-        {
-            get { return isVisible; }
-            set
-            {
-                isVisible = value;
-                UpdateVisibility(value);
-                OnPropertyChanged("IsVisible");
-            }
-        }
+//        public bool IsVisible
+//        {
+//            get { return isVisible; }
+//            set
+//            {
+//                isVisible = value;
+//                UpdateVisibility(value);
+//                OnPropertyChanged("IsVisible");
+//            }
+//        }
 
-        private Visibility visibility;
-        public Visibility Visibility
-        {
-            get { return visibility; }
-            set
-            {
-                visibility = value;
-                OnPropertyChanged("Visibility");
-            }
-        }
+//        private Visibility visibility;
+//        public Visibility Visibility
+//        {
+//            get { return visibility; }
+//            set
+//            {
+//                visibility = value;
+//                OnPropertyChanged("Visibility");
+//            }
+//        }
 
 
 
-        private void UpdateVisibility(bool isVisible)
-        {
-            if (isVisible)
-                Visibility = Visibility.Visible;
-            else
-                Visibility = Visibility.Hidden;
-        }
-    }
-}
+//        private void UpdateVisibility(bool isVisible)
+//        {
+//            if (isVisible)
+//                Visibility = Visibility.Visible;
+//            else
+//                Visibility = Visibility.Hidden;
+//        }
+//    }
+//}

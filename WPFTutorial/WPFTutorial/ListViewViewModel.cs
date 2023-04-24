1941f52a91650665eb71b2f4e064b320741d5321
hyunwoo <junghw86@quantamatrix.com>
2023-04-25 08:55:50 +0900

    ListViewModel에 직접 ICommand를 구현하여 연결 확인

--- WPFTutorial/WPFTutorial/ListViewViewModel.cs
+++ WPFTutorial/WPFTutorial/ListViewViewModel.cs
@@ -4,11 +4,15 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace WPFTutorial
 {
     class ListViewViewModel
     {
+        public ICommand RemoveNameCommand02 { get; set; }
+        public ICommand AddNameCommand02 { get; set; }
+
         public ObservableCollection<string> Names { get; set; }
         public ListViewViewModel()
         {
@@ -19,6 +23,21 @@ namespace WPFTutorial
                "Johen",
                "Matt"
            };
+
+            RemoveNameCommand02 = new RelayCommand<string>(RemoveName);
+            AddNameCommand02 = new RelayCommand<string>(AddName);
+        }
+
+        private void RemoveName(string name)
+        {
+            this.Names.Remove(name);
+        }
+
+        int count = 0;
+        private void AddName(string name)
+        {
+            count++;
+            this.Names.Add("add " + count.ToString());
         }
     }
 }
--- WPFTutorial/WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/WPFTutorial/MainWindow.xaml.cs
@@ -25,9 +25,5 @@ namespace WPFTutorial
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("??");
-        }
     }
 }

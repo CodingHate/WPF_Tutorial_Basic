b9635d5df66cdf72c8cb326ab948175d231165b6
hyunwoo <junghw86@quantamatrix.com>
2023-04-27 08:30:12 +0900

    user control 구성하여 tabcontrol 처럼 만듦

---
+++ WPFTutorial/WPFTutorial/Views/RedView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFTutorial.Views
+{
+    /// <summary>
+    /// RedView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class RedView : UserControl
+    {
+        public RedView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("click");
+        }
+    }
+}

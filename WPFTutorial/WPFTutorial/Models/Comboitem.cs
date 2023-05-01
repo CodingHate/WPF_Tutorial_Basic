ed86351b86cb40a438c70fd160531cda7ca1ed61
hyunwoo <junghw86@quantamatrix.com>
2023-05-01 23:04:57 +0900

    combobox의 ControlTemplate 확인 (Resource 전)

---
+++ WPFTutorial/WPFTutorial/Models/Comboitem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace WPFTutorial.Models
+{
+    class Comboitem
+    {
+        public Comboitem()
+        {
+
+        }
+
+        public string Value { get; set; } = "";
+        public Brush Brush { get; set; } = default;
+
+        public override string ToString()
+        {
+            return $"Value: {Value} / Brush: {Brush}";
+        }
+
+        public static List<Comboitem> Items => new List<Comboitem>
+        {
+            new Comboitem{Value="빨간색", Brush = Brushes.Red},
+            new Comboitem{Value="파란색", Brush = Brushes.Blue},
+            new Comboitem{Value="노란색", Brush = Brushes.Yellow},
+        };
+    }
+}
---
+++ WPFTutorial/WPFTutorial/ViewModels/MainViewModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFTutorial.Models;
+
+namespace WPFTutorial.ViewModels
+{
+    class MainViewModels
+    {
+        public MainViewModels()
+        {
+            Items = Comboitem.Items;
+        }
+
+        public List<Comboitem> Items { set; get; }
+    }
+}

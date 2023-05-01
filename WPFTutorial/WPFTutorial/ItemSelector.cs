0b3b5c57d8a4342099be689ced6f19eedc85f572
hyunwoo <junghw86@quantamatrix.com>
2023-05-01 10:18:34 +0900

    selector를 이용하여 list box 구성

---
+++ WPFTutorial/WPFTutorial/ItemSelector.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFTutorial
+{
+    class ItemSelector : DataTemplateSelector
+    {
+        public ItemSelector()
+        {
+
+        }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            Student? student = item as Student;
+            FrameworkElement? frameworkElement = container as FrameworkElement;
+
+            return (DataTemplate)frameworkElement.FindResource(
+                student?.Gender == "남"
+                ? "Man" : "Woman"
+                );
+        }
+    }
+}

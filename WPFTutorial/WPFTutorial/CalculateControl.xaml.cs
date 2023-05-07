640ebdb5ce9bfbc552ac87e1cde139dd1ad74e58
hyunwoo <junghw86@quantamatrix.com>
2023-05-08 08:02:27 +0900

    의존성 단계에 대해서 학습 중 (1) 입력을 받으면 UI에 바로 표시 된다.

---
+++ WPFTutorial/WPFTutorial/CalculateControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTutorial
+{
+    /// <summary>
+    /// CalculateControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class CalculateControl : UserControl
+    {
+        public CalculateControl()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public int Value1
+        {
+            get { return (int)GetValue(Value1Property); }
+            set { SetValue(Value1Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Value1.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty Value1Property =
+            DependencyProperty.Register("Value1", typeof(int), typeof(CalculateControl), new PropertyMetadata(0));
+
+
+    }
+}
